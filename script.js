e39ea3d5bc66cb948838a158657e36fc71b394ae
Colton Berger <coltonberger@optonline.net>
2018-04-10 14:44:40 -0700

    Added Solution

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 module.exports = {
-  difference: function(){
-
+  difference: function(num1, num2){
+    return num1 - num2
   },
-  isEqual: function(){
-
+  isEqual: function(a, b){
+    if(typeof(a) === typeof(b)) {
+      return a === b;
+    } else {
+      return false;
+    }
   },
-  isEven: function(){
-
+  isEven: function(number){
+    return number % 2 === 0
   },
-  isDivisible: function(){
-
+  isDivisible: function(a, b){
+    return a % b === 0;
   },
-  discountPercentage: function(){
-
+  discountPercentage: function(originalAmount, discount){
+    if (discount > 100 || discount < 0) {
+      return "please enter a number between 0 and 100"
+    } else {
+     return originalAmount * (discount / 100);
+    }
   },
-  isVowel: function(){
-
+  isVowel: function(string){
+    var vowels = "aeiou".split("");
+    return vowels.indexOf(string) > -1;
   },
-  celsiusToFahrenheit: function(){
-
+  celsiusToFahrenheit: function(ctemp){
+    return ctemp * 1.8 + 32;
   },
-  biggestOfThree: function(){
-
+  biggestOfThree: function(num1, num2, num3){
+    return Math.max(num1, num2, num3)
   },
   // Bonus
   federalIncomeTaxCalculator: function(){
